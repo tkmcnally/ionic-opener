b1bff2c64126c7efd662a382e71b4388b37428a5
Thomas McNally <tmcnally@techmatesgroup.com>
2016-07-04 12:38:18 -0400

    Initial check-in.

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('accountCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+    
